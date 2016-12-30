3e215c2d6bffd1a72bb5a7e36ec80c064fc112a7
prasetiady <dedy.berastagi@gmail.com>
2016-12-30 22:43:01 +0700

    :sparkles: Adding validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @example = {
+      title:        'Nothing',
+      description:  'What are you looking for!',
+      image_url:    'nothing.jpg',
+      price:        22.02
+    }
   end
 
   test "should get index" do
@@ -17,7 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: @example }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +40,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: @example }
     assert_redirected_to product_url(@product)
   end
 

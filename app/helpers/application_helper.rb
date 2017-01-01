082fe71dfb58ebb45de9718c32811da6d4e2004b
prasetiady <dedy.berastagi@gmail.com>
2017-01-02 05:43:20 +0700

    :package: Adding helper to hide div

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_dif_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display:none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end

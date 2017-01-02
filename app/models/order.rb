db6cae7dc1e4cae4b2c6ae8df853f914d661ecea
prasetiady <dedy.berastagi@gmail.com>
2017-01-02 10:58:27 +0700

    :rotating_light: Adding order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,6 @@ class Order < ApplicationRecord
     "Credit cart"    => 1,
     "Purchase order" => 2
   }
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
 end
